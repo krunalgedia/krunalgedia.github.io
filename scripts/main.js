b85e8d25360e54aa3ffaab6ff4000397e7adb26a
Krunal Gedia <krunalgedia@github.com>
2021-03-21 17:49:31 +0100

    files for first test webpage

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+document.querySelector('h1').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'file:///C:/Users/kbged/Downloads/web-projects/images/firefox.png') {
+      myImage.setAttribute('src','file:///C:/Users/kbged/Downloads/web-projects/images/firefox2.jpg');
+    } else {
+      myImage.setAttribute('src','file:///C:/Users/kbged/Downloads/web-projects/images/firefox.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
